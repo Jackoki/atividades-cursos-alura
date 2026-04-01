85fc9ac6611a7ae1a8bc1524e1952bd934cb94e2
Jackoki <kenji.inoue@hotmail.com>
2026-03-31 22:55:41 -0300

    Funcionamento de transformação de método de private para public além da realização de set.

--- curso-java-reflection/src/main/java/br/com/alura/refl/Transformator.java
+++ curso-java-reflection/src/main/java/br/com/alura/refl/Transformator.java
@@ -1,6 +1,8 @@
-package main.java.br.com.alura.refl;
+package br.com.alura.refl;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.lang.reflect.Field;
 
 public class Transformator {
     public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {        Class<?> source = input.getClass();
@@ -11,6 +13,25 @@ public class Transformator {
         Field[] sourceFields = source.getDeclaredFields();
         Field[] targetFields = target.getDeclaredFields();
 
+        Arrays.stream(sourceFields).forEach(sourceField -> 
+            Arrays.stream(targetFields).forEach(targetField -> {
+                validate(sourceField, targetField);
+                try {
+                    targetField.set(targetClass, sourceField.get(input));
+                }
+
+                catch(IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }));
+
         return targetClass;
+    }
+
+    private void validate(Field sourceField, Field targetField){
+        if(sourceField.getName().equals(targetField.getName()) && sourceField.getType().equals(targetField.getType())) {
+            sourceField.setAccessible(true);
+            targetField.setAccessible(true);
+        }
     }
 }
